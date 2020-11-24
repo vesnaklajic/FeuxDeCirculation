caa492d2b959ea75d34e683afd9fc82c2d2baa66
vesnaklajic <vesnaklajic77@gmail.com>
2020-11-24 18:03:54 +0100

    second commit

--- WebContent/main.js
+++ WebContent/main.js
@@ -2,31 +2,55 @@
  * 
  */
 var x = document.getElementsByClassName('colors');
+var y = document.getElementsByClassName('pi');
+var z = document.getElementById('droit');
 
-function feuxRouges(){
+function feuxRouges() {
 
-// status RED
-x[0].style.background = 'red';
-x[1].style.background = 'grey';
-x[2].style.background = 'grey';
-
-// status GREEN
-function runGr() {
-	x[0].style.background = 'grey';
+	// status RED
+	x[0].style.background = 'red';
 	x[1].style.background = 'grey';
-	x[2].style.background = 'green';
-}
-
-// status YELLOW
-function runY() {
-	x[0].style.background = 'grey';
-	x[1].style.background = 'yellow';
 	x[2].style.background = 'grey';
-}
+	z.style.background = 'grey';
+	y[0].style.background = 'grey';
+	y[1].style.background = 'green';
 
-var rx = setTimeout(runGr, 2000);
-var ry = setTimeout(runY, 5000);
-var start = setInterval(feuxRouges, 8000);
+	// change to GREEN
+	function runGr() {
+		x[0].style.background = 'grey';
+		x[1].style.background = 'grey';
+		x[2].style.background = 'green';
+		z.style.background = 'grey';
+	}
+
+	// change to YELLOW
+	function runY() {
+		x[0].style.background = 'grey';
+		x[1].style.background = 'yellow';
+		x[2].style.background = 'grey';
+	}
+	// change to right direction
+	function goRight() {
+		z.style.background = 'green';
+	}
+	//change to don't walk
+	function dontWalk() {
+		y[0].style.background = 'red';
+		y[1].style.background = 'grey';
+
+	}
+	//change to walk
+	function walk() {
+		y[0].style.background = 'grey';
+		y[1].style.background = 'green';
+
+	}
+	var gox= setTimeout(goRight,2500);
+	var piwalk= setTimeout(walk,3500);
+	var pidontw=setTimeout(dontWalk,7500);
+	var rx = setTimeout(runGr, 8000);
+	var ry = setTimeout(runY, 10000);
+	var start = setInterval(feuxRouges, 12000);
 }
 
 feuxRouges();
